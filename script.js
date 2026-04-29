5f6c01e9e89a6c7e2bc7d70a70c8bb8d2f1c76a1
andre_goudinho <andre_goudinho@estudante.sesisenai.org.br>
2026-04-29 17:35:55 -0300

    add - arrumei o login

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let cadastro = false;
 
-document.getElementById("toggle").onclick = () => {
+document.getElementById("toggle").onclick = (e) => {
+    e.preventDefault();
+
     cadastro = !cadastro;
+
     document.getElementById("titulo").innerText = cadastro ? "Cadastro" : "Login";
     document.querySelector("button").innerText = cadastro ? "Cadastrar" : "Entrar";
-    document.getElementById("toggle").innerText = cadastro ? "Já tem conta? Faça Login!" : "Não tem conta? Cadastre-se!";
+
+    document.getElementById("toggle").innerText =
+        cadastro ? "Já tem conta? Faça login!" : "Não tem conta? Cadastre-se!";
+
     document.getElementById("mensagem").innerHTML = "";
-}
+};
 
 document.getElementById("form-login").onsubmit = (e) => {
     e.preventDefault();
@@ -18,25 +24,27 @@ document.getElementById("form-login").onsubmit = (e) => {
     mensagem.innerHTML = "";
 
     if (!email.includes("@") || !email.includes(".")) {
-        mensagem.innerHTML = "<div class='erro'><p> Email Inválido!</p></div>";
+        mensagem.innerHTML = "<p class='text-danger'>Email inválido!</p>";
         return;
     }
 
     if (senha.length < 4) {
-        mensagem.innerHTML = "<div class='erro'><p> Senha muito curta!</p></div>";
+        mensagem.innerHTML = "<p class='text-danger'>Senha muito curta!</p>";
         return;
     }
 
     if (cadastro) {
         localStorage.setItem(email, senha);
-        mensagem.innerHTML = "<div class='sucesso'><p> Cadastrado co sucesso!</p></div>";
+        mensagem.innerHTML = "<p class='text-success'>Cadastro realizado!</p>";
     } else {
         let salva = localStorage.getItem(email);
+
         if (salva === senha) {
-            mensagem.innerHTML = "<div class='sucesso'><p>Login com sucesso!</p></div>";
+            mensagem.innerHTML = "<p class='text-success'>Login realizado!</p>";
         } else {
-            mensagem.innerHTML = "<div class='erro'><p>Dados Incorretos!</p></div>";
+            mensagem.innerHTML = "<p class='text-danger'>Dados incorretos!</p>";
         }
     }
+
     document.getElementById("form-login").reset();
-}
+};
